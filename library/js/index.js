c52d4c9fe78cf96e1fcdc713147617c0d33bfe57
Ria River <riria.river@gmail.com>
2023-09-03 13:59:08 +0300

    feat: implement logic of register, login, authorized and logout

--- library/js/index.js
+++ library/js/index.js
@@ -2,10 +2,57 @@
 import initDropdowns from './dropDownMenu.js';
 import initModals from './modal.js';
 import printSelfcheck from './selfcheck.js';
+import service from './service.js';
+import { getFormData } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const state = {
+    user: service.getCurrentUser(),
+  };
+
   initDropdowns();
   initModals();
+
+  // Actions
+  const register = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const modal = form.closest('.modal');
+    const formData = getFormData(form);
+
+    state.user = service.register(formData);
+
+    form.reset();
+    modal.close();
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const modal = form.closest('.modal');
+
+    const formData = getFormData(form);
+    state.user = service.login(formData);
+
+    form.reset();
+    modal.close();
+  };
+
+  const logout = () => {
+    state.user = service.logout();
+  };
+
+  // Set form listeners
+  const {
+    libraryCardForm, registerForm, loginForm, buyCardForm,
+  } = window;
+
+  registerForm.addEventListener('submit', register);
+  loginForm.addEventListener('submit', login);
+
+  // Set logout listener
+  const logoutBtn = document.querySelector('[data-action=logout]');
+  logoutBtn.addEventListener('click', logout);
 
   // Selfcheck
   printSelfcheck();
---
+++ library/js/service.js
@@ -0,0 +1,94 @@
+/* eslint-disable import/extensions */
+import { capitalize, getRandomIntInclusive } from './utils.js';
+
+const usersDB = 'libraryUsers(riariver)';
+const currentUser = 'libraryAuthorized(riariver)';
+
+const generateCardNumber = (base = 16, charCount = 9) => {
+  const min = base ** (charCount - 1);
+  const max = (base ** charCount) - 1;
+  return getRandomIntInclusive(min, max).toString(base);
+};
+
+// LocalStorage Interface
+const lsUtils = {
+  getData: (key) => JSON.parse(localStorage.getItem(key)),
+  setData: (key, data) => localStorage.setItem(key, JSON.stringify(data)),
+  deleteData: (key) => localStorage.removeItem(key),
+};
+
+// Imitation of Users DB
+const getUsers = () => lsUtils.getData(usersDB) || [];
+// eslint-disable-next-line max-len
+const findUser = (users, { email, cardNumber }) => users.find((user) => user.email === email || user.cardNumber === cardNumber);
+const isUserExist = (users, user) => !!findUser(users, { email: user.email });
+
+const userUtils = {
+  create: (userData) => ({
+    firstName: capitalize(userData.firstName.trim()),
+    lastName: capitalize(userData.lastName.trim()),
+    email: userData.email.trim().toLowerCase(),
+    password: userData.password,
+    cardNumber: generateCardNumber(),
+    hasCard: false,
+    visits: 1,
+    bonuses: 1240,
+    books: [],
+  }),
+
+  // eslint-disable-next-line no-param-reassign
+  increaseVisits: (user) => { user.visits += 1; return user; },
+
+  getData(user) {
+    const data = { ...user };
+    const secureProps = ['password', 'email'];
+
+    secureProps.forEach((prop) => delete data[prop]);
+    return data;
+  },
+};
+
+// Interface
+const service = {
+  setCurrentUser(user) { lsUtils.setData(currentUser, user.cardNumber); },
+
+  getCurrentUser() {
+    const userId = lsUtils.getData(currentUser);
+    if (!userId) return null;
+
+    const user = findUser(getUsers(), { cardNumber: userId });
+    return userUtils.getData(user);
+  },
+
+  register(data) {
+    const isDataCorrect = (data.firstName && data.lastName && data.email && data.password);
+    if (!isDataCorrect) return null;
+
+    const newUser = userUtils.create(data);
+    const users = getUsers();
+    if (isUserExist(users, newUser)) return null;
+
+    users.push(newUser);
+    this.setCurrentUser(newUser);
+
+    lsUtils.setData(usersDB, users);
+    return userUtils.getData(newUser);
+  },
+
+  login({ login, password }) {
+    const users = getUsers();
+    const user = findUser(users, { email: login.toLowerCase(), cardNumber: login.toLowerCase() });
+    if (!user || user.password !== password) return null;
+
+    userUtils.increaseVisits(user);
+    this.setCurrentUser(user);
+
+    lsUtils.setData(usersDB, users);
+    return userUtils.getData(user);
+  },
+
+  logout() { lsUtils.deleteData(currentUser); return null; },
+
+};
+
+export default service;
