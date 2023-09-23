ee6503cc285f281a6972915acaec78525cf91274
Ria River <riria.river@gmail.com>
2023-09-23 20:05:21 +0300

    update: add songs

--- audio-player/js/tracks.js
+++ audio-player/js/tracks.js
@@ -1,5 +1,17 @@
 export default [
   {
+    artist: 'Queen',
+    title: 'We Are The Champions',
+    url: 'assets/audio/Queen - We Are The Champions.mp3',
+    img: 'assets/img/queen.jpg',
+  },
+  {
+    artist: 'Amy MacDonald',
+    title: 'This Is The Life',
+    url: 'assets/audio/Amy MacDonald - This Is The Life.mp3',
+    img: 'assets/img/macdonald.jpg',
+  },
+  {
     artist: 'Placebo',
     title: 'The Never-ending Why',
     url: 'assets/audio/Placebo - The Never-ending Why.mp3',
@@ -12,9 +24,9 @@ export default [
     img: 'assets/img/zaz.jpg',
   },
   {
-    artist: 'Amy MacDonald',
-    title: 'This Is The Life',
-    url: 'assets/audio/Amy MacDonald - This Is The Life.mp3',
-    img: 'assets/img/macdonald.jpg',
+    artist: 'Killers',
+    title: 'Human',
+    url: 'assets/audio/Killers - Human.mp3',
+    img: 'assets/img/killers.jpg',
   },
 ];
