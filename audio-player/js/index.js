2d9818eabd8fb5e311167c002b849ad6fba97ebf
Ria River <riria.river@gmail.com>
2023-09-24 20:52:33 +0300

    refactor: change to getting data from json file

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -1,8 +1,14 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/extensions */
+import getData from './getData.js';
 import Player from './player.js';
-import data from './tracks.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const player = new Player('.player1', data);
-  player.init();
+  const url = 'tracks.json';
+
+  getData(url).then((data) => {
+    const player = new Player('.player1', data);
+    player.init();
+  });
 });
